6a208a0525090bb9e5b8f1048550845eca0853dc
ceci-lo <lcecilia8147@gmail.com>
2026-02-16 19:57:08 -0300

    se agrega codgo a mtricular y desmatriculasr alumno

--- src/sistemaacademico/SistemaAcademico.java
+++ src/sistemaacademico/SistemaAcademico.java
@@ -36,52 +36,60 @@ public class SistemaAcademico {
             System.out.println("Por favor ingrese el id (tres letras que especifican el nombre de la materia) y el nombre de la materia en sí\n");
             System.out.println("Ingrese el id :");
             String idMat = sc.nextLine();
-            
+
             System.out.println("Ingrese el nombre de la materia :");
             String nomMat = sc.nextLine();
-            
+
             Materia m1 = new Materia(idMat, nomMat);
             materias.add(m1);
 
             System.out.println("Usted ha creado la materia : " + nomMat + " exitosamente. \n");
-
-            MenuGestionarMateria();
-            int opcionSeleccionadaGM = sc.nextInt();
-            sc.nextLine();
-
-            //Matricular alumno
-            if (opcionSeleccionadaGM == 1) {
-
-                int nro;
-                do {
-                    MatricularAlumno(m1, sc);
-                    System.out.println("El alumno se ha matriculado exitosamente");
-                    System.out.println("¿Desea matricular otro alumno a esta materia?: \n1. si\n2. volver al menu GESTION MATERIA ");
-                    nro = sc.nextInt();
-                    sc.nextLine();
-                } while (nro == 1);
-
-                System.out.println("Finalizó la matriculación de alumnos.\n");
+            int opcionSeleccionadaGM;
+            do {
                 MenuGestionarMateria();
                 opcionSeleccionadaGM = sc.nextInt();
                 sc.nextLine();
+                //Matricular alumno
+                if (opcionSeleccionadaGM == 1) {
 
+                    int nro;
+                    do {
+                        MatricularAlumno(m1, sc);
+                        System.out.println("El alumno se ha matriculado exitosamente");
+                        System.out.println("¿Desea matricular otro alumno a esta materia?: \n1. si\n2. volver al menu GESTION MATERIA ");
+                        nro = sc.nextInt();
+                        sc.nextLine();
+                    } while (nro == 1);
+
+                    System.out.println("Finalizó la matriculación de alumnos.\n");
+                }
+                //Desmatricular alumnos
+            else if (opcionSeleccionadaGM == 2) {
+                        System.out.println("Ingrese el nro de legajo del alumno a desmatricular");
+                        int nroLegajo = sc.nextInt();
+                        sc.nextLine();
+
+                        DesmatricularAlumnos(nroLegajo, m1);
+                         System.out.println("El alumno ha sido desmatriculado exitosamente");
+
+                        }
+              else if (opcionSeleccionadaGM == 4) {
+                System.out.println("\n--- ALUMNOS INSCRIPTOS EN " + m1.getNombreMateria() + " ---");
+                m1.mostrarListadosInscriptos();
+                System.out.println();
             }
-            //Desmatricular alumnos
-        }else if (opcionSeleccionada == 2) {
-            int nroLegajo = sc.nextInt();
-            sc.nextLine();
-            DesmatricularAlumno(nroLegajo, m1);
-            System.out.println("Usted ha salido del menu inicio. Muchas Gracias.");
+            //Opcion Salir
+            else if (opcionSeleccionada == 9) {
 
-        } else if (opcionSeleccionada == 9) {
+                        System.out.println("Usted ha salido del menu inicio. Muchas Gracias.");
 
-            System.out.println("Usted ha salido del menu inicio. Muchas Gracias.");
-
+                    }
         }
+        while (opcionSeleccionada != 9);
         sc.close();
     }
-
+    }
+    
     public static void MenuGestionarMateria() {
         System.out.println("MENÚ GESTIONAR MATERIA\n"
                 + "==========================\n"
@@ -125,14 +133,14 @@ public class SistemaAcademico {
 
             Alumno a = new Alumno(nroLegajo, apellido, nombre, fecha, email);
             materia.matricularAlumno(a);
-            materia.alumnosMatriculados();
+            
 
         } catch (ParseException ex) {
             Logger.getLogger(SistemaAcademico.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
-    public static void DesmatricularAlumnos(int nroLegajo, Materia materia){
+
+    public static void DesmatricularAlumnos(int nroLegajo, Materia materia) {
         materia.desmatricularAlumno(nroLegajo);
         System.out.println("El alumno ha sido desmatriculado exitosamente");
     }
